a3e2e0cb7136ba12964edf0657c49894ff67aa80
fHulk23 <alancayfabianarmando@gmail.com>
2026-02-17 19:25:05 -0300

    ruta a la vue regional

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,23 @@
-import InspecciondeObra from '@/components/InspecciondeObra.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
+import InspecciondeObra from '@/components/InspecciondeObra.vue'
+import Regionales from '@/components/Regionales.vue' 
 
 const routes = [
-    {
-      path: '/InspecciondeObra',
-      name: 'InspecciondeObra',
-      component: InspecciondeObra,
-    }
-  ];
-  
-  const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL), 
-    routes,
-  });
-  
-  export default router;
+  {
+    path: '/InspecciondeObra',
+    name: 'InspecciondeObra',
+    component: InspecciondeObra,
+  },
+  {
+    path: '/regionales', 
+    name: 'Regionales',
+    component: Regionales,
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+export default router
